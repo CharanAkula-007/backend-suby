fe67ea3489f9799efc40c9e0732b239966002ae5
CharanAkula-007 <charanakula63@gmail.com>
2025-08-05 20:59:14 +0530

    adding cors package

--- index.js
+++ index.js
@@ -1,36 +1,37 @@
-
-const express = require('express');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-const vendorRoutes = require('./routes/vendorRoutes');
-const firmRoutes = require('./routes/firmRoutes');
-const productRoutes = require('./routes/productRoutes');
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const vendorRoutes = require("./routes/vendorRoutes");
+const firmRoutes = require("./routes/firmRoutes");
+const productRoutes = require("./routes/productRoutes");
+const cors = require("cors");
 // const productRoutes = require('./routes/productRoutes');
 
 const app = express();
 const port = 3000;
 
 dotenv.config();
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-    console.log("Connected to MongoDB successfully"); 
-})
-.catch((err) => {
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB successfully");
+  })
+  .catch((err) => {
     console.error("Error connecting to MongoDB:", err);
-});
-
+  });
+app.use(cors());
 app.use(express.json());
 
-app.use('/vendor', vendorRoutes);
+app.use("/vendor", vendorRoutes);
 // app.use('/product', productRoutes);
 
-app.use('/firm', firmRoutes);
+app.use("/firm", firmRoutes);
 
-app.use('/product', productRoutes);
+app.use("/product", productRoutes);
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
 
-app.use('/', (req, res)=> {
-    res.send("<h1>Welcome to Charan's project</h1>");
-})
+app.use("/", (req, res) => {
+  res.send("<h1>Welcome to Charan's project</h1>");
+});
